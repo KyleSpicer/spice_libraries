8809139e246cf12b4bd3b70227898826a7497328
Kyle Spicer <kyle.d.spicer.mil@army.mil>
2022-12-23 18:13:16 -0500

    Update linux kernel style

--- c/stack/stack.h
+++ c/stack/stack.h
@@ -3,30 +3,27 @@
 
 typedef struct stack_node_t stack_node_t;
 
-struct stack_node_t
-{
-    int                   data;
-    struct stack_node_t * next;
+struct stack_node_t {
+	int data;
+	struct stack_node_t *next;
 };
 
 typedef struct stack_t stack_t;
 
-stack_t * stack_create();
+stack_t *stack_create();
 
 void stack_push(stack_t * stack, int data);
 
 int is_stack_empty(stack_t * stack);
 
-stack_node_t * stack_pop(stack_t * stack);
+stack_node_t *stack_pop(stack_t * stack);
 
-stack_node_t * stack_peek(stack_t * stack);
+stack_node_t *stack_peek(stack_t * stack);
 
 void stack_display(stack_t * stack);
 
 void stack_destroy(stack_t ** stack);
 
-
-
-#endif /* STACK_H */
+#endif				/* STACK_H */
 
 /*** end of file ***/
