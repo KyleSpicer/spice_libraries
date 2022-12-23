5e560048bd06a23fa8105ae24eb596eb62baf9b2
Kyle Spicer <kyle.d.spicer.mil@army.mil>
2022-12-23 18:12:38 -0500

    Update linux kernel style

--- c/queue/queue.c
+++ c/queue/queue.c
@@ -4,142 +4,123 @@
 
 #include "queue.h"
 
-struct queue_t
-{
-    int   head;
-    int   tail;
-    int   size;
-    int * data;
+struct queue_t {
+	int head;
+	int tail;
+	int size;
+	int *data;
 };
 
-queue_t *
-queue_create(int size)
+queue_t *queue_create(int size)
 {
-    queue_t * queue = calloc(1, sizeof(*queue));
- 
-    if (queue)
-    {
-        queue->size = size;
-        queue->data = calloc(size, sizeof(int));
-        if (!queue->data)
-        {
-            free(queue);
-            queue = NULL;
-        }
-    }
+	queue_t *queue = calloc(1, sizeof(*queue));
 
-    return queue;
+	if (queue) {
+		queue->size = size;
+		queue->data = calloc(size, sizeof(int));
+		if (!queue->data) {
+			free(queue);
+			queue = NULL;
+		}
+	}
+
+	return queue;
 }
 
-void
-queue_destroy(queue_t ** queue)
+void queue_destroy(queue_t ** queue)
 {
-    if (!queue || !*queue)
-    {
-        return;
-    }
+	if (!queue || !*queue) {
+		return;
+	}
 
-    free((*queue)->data);
-    free(*queue);
-    *queue = NULL;
+	free((*queue)->data);
+	free(*queue);
+	*queue = NULL;
 }
 
-void
-reset_queue(queue_t * queue)
+void reset_queue(queue_t * queue)
 {
-    if (!queue)
-    {
-        return;
-    }
-    queue->head = 0;
-    queue->tail = 0;
+	if (!queue) {
+		return;
+	}
+	queue->head = 0;
+	queue->tail = 0;
 }
 
-queue_t *
-queue_enqueue(queue_t * queue, int value)
+queue_t *queue_enqueue(queue_t * queue, int value)
 {
-    // avoids negative numbers, or max capacity reached
-    if (!queue || value < 0 || queue->tail == queue->size)
-    {
-        if (!queue)
-        {
-            printf("bad queue\n");
-        }
-        else if (value < 0)
-        {
-            printf("bad value\n");
-        }
-        else
-        {
-            printf("QUEUE IS FULL!\n");
-            printf("%d == %d\n", queue->tail, queue->size);
-        }
-        return NULL;
-    }
+	// avoids negative numbers, or max capacity reached
+	if (!queue || value < 0 || queue->tail == queue->size) {
+		if (!queue) {
+			printf("bad queue\n");
+		} else if (value < 0) {
+			printf("bad value\n");
+		} else {
+			printf("QUEUE IS FULL!\n");
+			printf("%d == %d\n", queue->tail, queue->size);
+		}
+		return NULL;
+	}
 
-    queue->data[queue->tail++] = value;
+	queue->data[queue->tail++] = value;
 
-    return queue;
+	return queue;
 }
 
-int
-queue_dequeue(queue_t * queue)
+int queue_dequeue(queue_t * queue)
 {
-    if (!queue || queue->head == queue->tail)
-    {
-        return -1;
-    }
+	if (!queue || queue->head == queue->tail) {
+		return -1;
+	}
 
-    return queue->data[queue->head++];
+	return queue->data[queue->head++];
 }
 
-int
-array_int_sort(const void * data1, const void * data2)
+int array_int_sort(const void *data1, const void *data2)
 {
-    if (!data1 || !data2)
-    {
-        printf("insufficient data entered.\n");
-        return 1;
-    }
-    return *(int *)data1 - *(int *)data2;
+	if (!data1 || !data2) {
+		printf("insufficient data entered.\n");
+		return 1;
+	}
+	return *(int *)data1 - *(int *)data2;
 }
 
-void queue_print(queue_t * queue) {
-    if (!queue) {
-        printf("Unable to print queue.\n");
-        return;
-    }
+void queue_print(queue_t * queue)
+{
+	if (!queue) {
+		printf("Unable to print queue.\n");
+		return;
+	}
 
-    for (int i = queue->head; i < queue->tail; i++) {
-        printf("%d ", queue->data[i]);
-    }
-    printf("\n");
+	for (int i = queue->head; i < queue->tail; i++) {
+		printf("%d ", queue->data[i]);
+	}
+	printf("\n");
 }
 
 bool is_queue_empty(queue_t * queue)
 {
-    if(!queue){
-        return false;
-    }
-    if(0 == queue->size){
-        return true;
-    }
-    return false;
+	if (!queue) {
+		return false;
+	}
+	if (0 == queue->size) {
+		return true;
+	}
+	return false;
 }
 
 bool is_queue_full(queue_t * queue, int max_queue_size)
 {
-    if(!queue){
-        return NULL;
-    }
-    printf("q size = %d\n", queue->size);
-    printf("max size = %d\n", max_queue_size);
+	if (!queue) {
+		return NULL;
+	}
+	printf("q size = %d\n", queue->size);
+	printf("max size = %d\n", max_queue_size);
 
-    if(queue->size == max_queue_size) {
-        return true;
-    }
-    return false;
+	if (queue->size == max_queue_size) {
+		return true;
+	}
+	return false;
 }
 
 /*** end of file ***/
-
--- c/queue/queue.h
+++ c/queue/queue.h
@@ -3,17 +3,17 @@
 
 typedef struct queue_t queue_t;
 
-queue_t * queue_create(int size);
+queue_t *queue_create(int size);
 
 void queue_destroy(queue_t ** queue);
 
 void reset_queue(queue_t * queue);
 
-queue_t * queue_enqueue(queue_t * queue, int value);
+queue_t *queue_enqueue(queue_t * queue, int value);
 
 int queue_dequeue(queue_t * queue);
 
-int array_int_sort(const void * data1, const void * data2);
+int array_int_sort(const void *data1, const void *data2);
 
 void queue_print(queue_t * queue);
 
@@ -21,6 +21,4 @@ bool is_queue_empty(queue_t * queue);
 
 bool is_queue_full(queue_t * queue, int max_queue_size);
 
-
-
-#endif /* QUEUE_H */
+#endif				/* QUEUE_H */
