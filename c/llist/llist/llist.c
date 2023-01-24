a86153b9e8859c907ba1f6e9276b9a7381e1705a
Kyle Spicer <kyle.d.spicer.mil@army.mil>
2023-01-24 06:47:39 -0500

    Update basic llist funcs

--- c/llist/llist/llist.c
+++ c/llist/llist/llist.c
@@ -1,14 +1,9 @@
 #include "llist.h"
+#include "bacon_funcs.h"
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
-
-struct llist_t {
-	llist_node_t *head;
-	llist_node_t *tail;
-	uint64_t llist_size;
-};
 
 llist_t *llist_create()
 {
@@ -17,6 +12,8 @@ llist_t *llist_create()
 		return NULL;
 	}
 	llist->head = NULL;
+	llist->tail = NULL;
+	llist->count = 0;
 	return llist;
 }
 
@@ -31,47 +28,106 @@ int is_llist_empty(llist_t * llist)
 	if (!llist) {
 		return -1;
 	}
-	if (llist->llist_size > 0) {
-		return 1;
+	if (llist->count > 0) {
+		return 0;
 	}
-	return 0;
+	return 1;
 }
 
-void llist_insert_front(llist_t * llist, int data)
+void llist_insert_front(llist_t * llist, void *data)
 {
 	if (!llist || !data) {
 		return;
 	}
-	llist_node_t *new_node = calloc(1, sizeof(llist_node_t));
-	llist->llist_size++;
+	llist_element_t *new_node = calloc(1, sizeof(llist_element_t));
+	if (!new_node) {
+		return;
+	}
 	new_node->data = data;
+
+	if (!llist->tail) {
+		llist->tail = new_node;
+	}
 	new_node->next = llist->head;
 	llist->head = new_node;
+	llist->count++;
 }
 
-void llist_insert_back(llist_t * llist, int data)
+void llist_actor_insert_sort(llist_t * llist, void *data)
 {
 	if (!llist || !data) {
 		return;
 	}
-
-	llist_node_t *new_node = calloc(1, sizeof(llist_node_t));
-	new_node->data = data;
+	llist_element_t *new_node = calloc(1, sizeof(llist_element_t));
 	if (!new_node) {
 		return;
 	}
 
-	if (llist->tail) {
-		llist->tail->next = new_node;
+	new_node->data = data;
+
+	if (!llist->tail) {
 		llist->tail = new_node;
-	} else {
+		new_node->next = NULL;
 		llist->head = new_node;
-		llist->tail = new_node;
+		llist->count++;
+		return;
 	}
-	llist->llist_size++;
+	// compare new actor to head
+	llist_element_t *curr = llist->head;
+	actor_t *curr_actor = curr->data;
+
+	actor_t *new_actor = new_node->data;
+
+	int first_result = compare_actor_names(curr_actor, new_actor);
+	if (first_result > 0) {
+		// printf("INSERTING AT FRONT\n");
+		llist_insert_front(llist, new_actor);
+		return;
+	}
+
+	while (curr->next) {
+		actor_t *next_actor = curr->next->data;
+		int ret = compare_actor_names(next_actor, new_actor);
+
+		if (0 < ret) {
+			break;
+		} else {
+			curr = curr->next;
+		}
+	}
+	new_node->next = curr->next;
+	curr->next = new_node;
+	llist->count++;
 }
 
-llist_node_t *llist_remove_front(llist_t * llist)
+void llist_insert_back(llist_t * llist, void *data)
+{
+	if (!llist || !data) {
+		return;
+	}
+
+	llist_element_t *new_node = calloc(1, sizeof(llist_element_t));
+	if (!new_node) {
+		return;
+	}
+
+	new_node->data = data;
+	new_node->next = NULL;
+
+	if (NULL == llist->head) {
+		llist->head = new_node;
+	} else {
+		llist_element_t *current = llist->head;
+
+		while (NULL != current->next) {
+			current = current->next;
+		}
+		current->next = new_node;
+	}
+	llist->count++;
+}
+
+llist_element_t *llist_remove_front(llist_t * llist)
 {
 	if (!llist) {
 		return NULL;
@@ -79,59 +135,63 @@ llist_node_t *llist_remove_front(llist_t * llist)
 		printf("linked list is empty.\n");
 		return NULL;
 	}
-	llist_node_t *temp = llist->head;
+	llist_element_t *temp = llist->head;
 	llist->head = temp->next;
-
-	return (temp);
-
+	llist->count--;
+	llist_element_t *data = temp->data;
+	free(temp);
+	return (data);
 }
 
-llist_node_t *llist_remove_back(llist_t * llist)
-{
-	if (!llist) {
-		return NULL;
-	}
+// llist_node_t *llist_remove_back(llist_t * llist)
+// {
+//      if (!llist) {
+//              return NULL;
+//      }
 
-	else if (is_llist_empty(llist)) {
-		return NULL;
-	}
-	// if only head element exists
-	else if (llist->head == NULL) {
-		llist_node_t *temp_head = llist->head;
-		free(llist->head);
-		return temp_head;
-	}
+//      else if (is_llist_empty(llist)) {
+//              return NULL;
+//      }
+//      // if only head element exists
+//      else if (llist->head == NULL) {
+//              llist_node_t *temp_head = llist->head;
+//              free(llist->head);
+//              return temp_head;
+//      }
 
-	llist_node_t *curr = llist->head;
-	while (curr->next->next != NULL) {
-		curr = curr->next;
-	}
+//      llist_node_t *curr = llist->head;
+//      while (curr->next->next != NULL) {
+//              curr = curr->next;
+//      }
 
-	free(curr->next);
-	curr->next = NULL;
+//      free(curr->next);
+//      curr->next = NULL;
 
-	return curr;
-}
+//      return curr;
+// }
 
-void llist_display(llist_t * llist)
-{
-	if (!llist) {
-		return;
-	}
-	llist_node_t *curr = llist->head;
-	while (curr != NULL) {
-		printf("%d ", curr->data);
-		curr = curr->next;
-	}
-	printf("\n");
-}
+// void llist_display(llist_t * llist)
+// {
+//      if (!llist) {
+//              return;
+//      }
+//      llist_node_t *curr = llist->head;
+//      int station_count = 1;
+//      while (curr != NULL) {
+//              printf("Stop %2d: (x: %lf, y: %lf)\n",
+//                     station_count, curr->node->x_coord, curr->node->y_coord);
+//              curr = curr->next;
+//              station_count++;
+//      }
+//      printf("\n");
+// }
 
 int llist_size(llist_t * llist)
 {
 	if (!llist) {
 		return -1;
 	}
-	return llist->llist_size;
+	return llist->count;
 }
 
 void llist_destroy(llist_t ** llist)
@@ -139,7 +199,7 @@ void llist_destroy(llist_t ** llist)
 	if (!*llist || !llist) {
 		return;
 	}
-	llist_node_t *temp = (*llist)->head;
+	llist_element_t *temp = (*llist)->head;
 	while (temp) {
 		(*llist)->head = temp->next;
 		free(temp);
--- c/llist/llist/llist.h
+++ c/llist/llist/llist.h
@@ -1,31 +1,99 @@
 #ifndef LLIST_H
 #define LLIST_H
 
-typedef struct llist_node_t llist_node_t;
+#include <stdint.h>
 
-struct llist_node_t {
-	int data;
-	llist_node_t *next;
-};
+// #include "bacon_funcs.h"
 
-typedef struct llist_t llist_t;
+typedef struct llist_element llist_element_t;
+/**
+ * @brief structure for each node within linked list
+ * 
+ */
+typedef struct llist_element {
+	void *data;
+	llist_element_t *next;
+} llist_element_t;
+
+typedef struct llist llist_t;
+
+typedef struct llist {
+	llist_element_t *head;	// same thing as an element
+	llist_element_t *tail;	// same thing as an element
+	uint64_t count;		// number of elements
+} llist_t;
+
+/**
+ * @brief allocates memory to begin populating linked list
+ * 
+ * @return llist_t* 
+ */
 
 llist_t *llist_create();
 
+/**
+ * @brief checks the amount of nodes within llist
+ * 
+ * @param llist 
+ * @return int - the amount of nodes within the llist
+ */
 int is_llist_empty(llist_t * llist);
 
-void llist_insert_front(llist_t * llist, int data);
+/**
+ * @brief inserts a llist node into the front of a given llist
+ * 
+ * @param llist 
+ * @param data 
+ */
+void llist_insert_front(llist_t * llist, void *data);
 
-void llist_insert_back(llist_t * llist, int data);
+void llist_actor_insert_sort(llist_t * llist, void *data);
 
-llist_node_t *llist_remove_front(llist_t * llist);
+/**
+ * @brief inserts a llist node into the back of a given llist
+ * 
+ * @param llist 
+ * @param data 
+ */
+void llist_insert_back(llist_t * llist, void *data);
 
-llist_node_t *llist_remove_back(llist_t * llist);
+/**
+ * @brief removes front node from a given llist
+ * 
+ * @param llist 
+ * @return llist_node_t* 
+ */
+llist_element_t *llist_remove_front(llist_t * llist);
 
+/**
+ * @brief removes end node from given llist
+ * 
+ * @param llist 
+ * @return llist_node_t* 
+ */
+llist_element_t *llist_remove_back(llist_t * llist);
+
+/**
+ * @brief prints contents within llist according to function logic
+ * 
+ * @param llist 
+ */
 void llist_display(llist_t * llist);
 
+/**
+ * @brief returns the amount of nodes within a given llist
+ * 
+ * @param llist 
+ * @return int 
+ */
 int llist_size(llist_t * llist);
 
+/**
+ * @brief frees all memory allocation and sets container memory address to NULL
+ * 
+ * @param llist 
+ */
 void llist_destroy(llist_t ** llist);
 
 #endif				/* LLIST_H */
+/*** end of file ***/
