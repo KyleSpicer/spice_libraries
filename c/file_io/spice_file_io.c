5daa582c2b0c69263fa6252058005f3f47b43e46
Kyle Spicer <kyle.d.spicer.mil@army.mil>
2023-01-24 06:47:16 -0500

    Update file io funcs

--- c/file_io/spice_file_io.c
+++ c/file_io/spice_file_io.c
@@ -1,6 +1,27 @@
-#include "spice_file_io.h"
+/**
+ * @file spice_file_io.c
+ * @author CW2 Kyle Spicer
+ * @brief library that contains functions for file input/output operations.
+ * 
+ * @date 2023-01-09
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ * @cite 1. Utilized the following link to understand how to properly implement 
+ * 		 sys/stat.h to capture file information : 
+ * 		 https://en.wikibooks.org/wiki/C_Programming/POSIX_Reference/sys/stat.h
+ * 
+ */
+
+#include <ctype.h>
+#include <limits.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+
+#include "spice_file_io.h"
 
 static int file_verify_not_empty(FILE * fp)
 {
@@ -10,6 +31,7 @@ static int file_verify_not_empty(FILE * fp)
 
 	// file is empty
 	if (0 == size) {
+		printf("File is empty.\n");
 		return 0;
 	}
 
@@ -28,7 +50,6 @@ FILE *file_open_and_verify(char *file_name)
 
 	FILE *file = fopen(file_name, "r");
 	if (!file) {
-		printf("Unable to open %s\n", file_name);
 		return NULL;
 	}
 
@@ -41,28 +62,159 @@ FILE *file_open_and_verify(char *file_name)
 	}
 }
 
+/**
+ * @brief function to determine the size of the file in bytes.
+ * 
+ * @param file_name 
+ * @return int - returns size of bytes or -1 on error.
+ */
+int file_size_bytes(char *file_name)
+{
+	if (!file_name) {
+		return -1;
+	}
+
+	struct stat file_stats;
+
+	// stat() returns 0 on successful operation
+	if (0 == stat(file_name, &file_stats)) {
+		return file_stats.st_size;
+	}
+
+	return -1;
+}
+
+int file_line_count(char *file_name)
+{
+	if (!file_name) {
+		return -1;
+	}
+
+	char buff[256];
+	int line_count = 0;
+
+	FILE *fp = fopen(file_name, "r");
+
+	if (!fp) {
+		return -1;
+	}
+	// using fgets to determine number of lines within file
+	while (NULL != fgets(buff, 256, fp)) {
+		line_count++;
+	}
+
+	fclose(fp);
+
+	return line_count;
+}
+
+/**
+ * @brief find
+ * 
+ * @param filename 
+ * @return int 
+ */
+int file_line_integers_begin(char *filename)
+{
+
+	if (!filename) {
+		return -1;
+	}
+
+	char buff[256];
+	int line_count = 0;
+
+	FILE *fp = fopen(filename, "r");
+	while (NULL != fgets(buff, 256, fp)) {
+		line_count++;
+		if (isdigit(*buff)) {
+			break;
+		}
+	}
+	fclose(fp);
+	return line_count;
+
+}
+
+/**
+ * @brief function that reads the size (in bytes) of only the first line in a file
+ * 
+ * @param file_name 
+ * @return int - returns size of first line in file, in bytes
+ */
+int size_of_first_line(char *file_name)
+{
+	if (!file_name) {
+		return -1;
+	}
+
+	int c = 0;
+
+	FILE *fp = fopen(file_name, "r");
+	if (!fp) {
+		return -1;
+	}
+	while ((c = fgetc(fp)) != '\n' && c != EOF) {
+		c++;
+	}
+
+	fclose(fp);
+	return c;
+
+}
+
+/**
+ * @brief displays informative usage message to user
+ * 
+ */
 void display_usage_statement(void)
 {
 	const char *message = "\n---------- Program Help Menu ----------\n"
-	    "\nUsage: ./exe [-f] <file> [-arg...] <argument(s)>\n"
-	    "\nMandatory Arguments:\n"
-	    "\t-f | --file     : input file to use with program.\n"
-	    "\t-x | --lat 	: provide x coordinate for use in program.\n"
-	    "\t-y | --lon 	: provide y coordinate for use in program.\n"
-	    "\t-d | --distance : provide radius to conduct search.\n"
-	    "\nOptional Arguments: \n"
-	    "\t-k | --nodes   : returns number of closest nodes to coordinates provided.\n"
-	    "\t-h | --help	: displays all options and usage of program.\n"
-	    "\nExample Usage:\n"
-	    "\tShort Options: \n"
-	    "\t\t./myprog -f file.txt -x 2.5 -y 3.0 -d 10\n"
-	    "\t\t./myprog -f file.txt -x 2.5 -y 3.0 -d 10 -n 3\n"
-	    "\n\tLong Options :\n"
-	    "\t\t./myprog --file file.txt --lat 2.5 --lon 3.0 --distance 10\n"
-	    "\t\t./myprog --file file.txt --lat 2.5 --lon 3.0 --distance 10 --number 3\n\n"
-	    "------------ End Help Menu ------------\n\n";
+	    "\nUsage: ./bacon -f [file] -args [argument format] \n"
+	    "\nCommand Line Arguments:\n"
+	    "\n\tCOMMAND \t   ARGUMENT FORMAT \t DESCRIPTION\n"
+	    "\t-f / --file \t   \"input file\" \t (alternate input file)\n"
+	    "\t-c / --cast \t   \"Movie Title\" YEAR \t (shows cast of movie)\n"
+	    "\t-m / --movieinfo   \"Movie Title\" YEAR \t (show rating/genre of movie)\n"
+	    "\t-r / --roles \t   \"Actor\" \t\t (displays films actor has been in)\n"
+	    "\t-s / --stats \t   \"Actor\" \t\t (connectivity of actor)\n"
+	    "\t-d / --distance    \"Actor A\" \"Actor B\" \t (displays actor->movie->actor relationship)\n"
+	    "\n\t-f / --help \t (shows informative help menu)\n"
+	    "\nExample Usages:\n"
+	    "\t ./bacon -f file.txt -c \"The Hangover\" 2008 \n"
+	    "\t ./bacon -f file.txt --roles \"Adam Sandler\" \n"
+	    "\t ./bacon -f file.txt -d \"Adam Sandler\" \"Kevin Bacon\" \n"
+	    "\n------------ End Help Menu ------------\n\n";
 
-	printf("%s", message);
+	fprintf(stdout, "%s", message);
+}
+
+/**
+ * @brief Accepts a string of potential year and verifies it is a legitimate
+ * four digit year within a specified range.
+ * 
+ * @param string_of_int - string of potential year
+ * @return true - if good year
+ * @return false - if bad year
+ */
+bool check_good_year(char *string_of_int)
+{
+	if (!string_of_int) {
+		return false;
+	}
+	if (!isdigit(*string_of_int)) {
+		return false;
+	}
+	if (4 != strlen(string_of_int)) {
+		return false;
+	}
+	char *remaining = NULL;
+	long year = 0;
+	year = strtol(string_of_int, &remaining, 10);
+	if (1825 > year || 2023 < year) {
+		return false;
+	}
+	return true;
 }
 
 /* end of file */
--- c/file_io/spice_file_io.h
+++ c/file_io/spice_file_io.h
@@ -1,11 +1,80 @@
+/**
+ * @file spice_file_io.h
+ * @author CW2 Kyle Spicer
+ * @brief header file that contains declarations and doc strings for functions
+ * within spice_file_io.c
+ * @version 0.1
+ * @date 2023-01-09
+ * @cite - if there are citations, they are located within the doc string for 
+ * specific function
+ * @copyright Copyright (c) 2023
+ * 
+ */
 #ifndef SPICE_FILE_IO
 #define SPICE_FILE_IO
 
 #include <stdio.h>
 
+/**
+ * @brief opens and checks file for the following: if file exists, open file, 
+ * verify file isn't empty by retrieving file size. Responsibility of user to
+ * properly close file.
+ * 
+ * 
+ * @param file_name 
+ * @return FILE* - returns file pointer
+ */
 FILE *file_open_and_verify(char *file_name);
 
+/**
+ * @brief utilizes sys/stat.h to check file size in bytes
+ * file is closed prior to function terminating
+ * 
+ * @param file_name 
+ * @return int 
+ */
+int file_size_bytes(char *file_name);
+
+/**
+ * @brief cycles through all lines in file and returns the count found
+ * file is closed prior to function terminating
+ * 
+ * @param file_name 
+ * @return int 
+ */
+int file_line_count(char *file_name);
+
+/**
+ * @brief specific function to determine first line in file that has integers
+ * 
+ * @param filename 
+ * @return int - returns line number that has integers (ex. coordinates in csv)
+ */
+int file_line_integers_begin(char *filename);
+
+/**
+ * @brief prints informative usage statement
+ * 
+ */
 void display_usage_statement(void);
+
+/**
+ * @brief reads the first line of a CSV file and returns the amount of bytes
+ * 
+ * @param file_name 
+ * @return int 
+ */
+int size_of_first_line(char *file_name);
+
+/**
+ * @brief Accepts a string of potential year and verifies it is a legitimate
+ * four digit year within a specified range.
+ * 
+ * @param string_of_int - string of potential year
+ * @return true - if good year
+ * @return false - if bad year
+ */
+bool check_good_year(char *string_of_int);
 
 #endif				/* SPICE_FILE_IO */
 /* end of file */
